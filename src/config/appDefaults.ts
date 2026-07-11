f4652f8bae10b53298822c206ba12dce6ad6b60a
MehdiAlireza <max.dlearning@gmail.com>
2026-07-11 10:50:21 +0330

    Create appDefaults.ts

---
+++ src/config/appDefaults.ts
@@ -0,0 +1,25 @@
+export const APP_DEFAULTS = {
+
+    appName: "MAGENAIS",
+
+    version: "2.2",
+
+    autoHealthCheck: true,
+
+    autoRepair: true,
+
+    autoFallback: true,
+
+    autoLoadProviders: true,
+
+    autoSaveProviders: true,
+
+    requestTimeout: 30000,
+
+    retryCount: 2,
+
+    parallelProviders: false,
+
+    debugMode: false
+
+};
