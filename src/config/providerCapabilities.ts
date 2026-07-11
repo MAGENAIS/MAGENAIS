0b45a82b665a1ad1402cacc78a319bf6ec9fccc1
MehdiAlireza <max.dlearning@gmail.com>
2026-07-11 10:50:59 +0330

    Create providerCapabilities.ts

---
+++ src/config/providerCapabilities.ts
@@ -0,0 +1,29 @@
+export enum Capability {
+
+    TEXT="text",
+
+    IMAGE="image",
+
+    VIDEO="video",
+
+    AUDIO="audio",
+
+    MUSIC="music",
+
+    SPEECH="speech",
+
+    CHAT="chat",
+
+    OCR="ocr",
+
+    DOCUMENT="document",
+
+    SEARCH="search",
+
+    RESEARCH="research",
+
+    AGENT="agent",
+
+    GAME="game"
+
+}
