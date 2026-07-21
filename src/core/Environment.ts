2389ef59173982cc8e52eabdb1172999953c15cd
MehdiAlireza <max.dlearning@gmail.com>
2026-07-21 09:21:33 +0330

    Update Environment.ts

--- src/core/Environment.ts
+++ src/core/Environment.ts
@@ -1,13 +1,9 @@
 export const Environment = {
 
-    isGitHubPages:
-        location.hostname.endsWith("github.io"),
+  isGitHubPages:
+    window.location.hostname.endsWith("github.io"),
 
-    isLocalhost:
-        location.hostname === "localhost" ||
-        location.hostname === "127.0.0.1",
-
-    hasServerProxy:
-        !(location.hostname.endsWith("github.io")),
+  hasServerProxy:
+    !window.location.hostname.endsWith("github.io"),
 
 };
